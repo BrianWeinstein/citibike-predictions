6b454bd8010535ea88d1cdf5c44240c252b26748
BrianWeinstein <bweinstein02@gmail.com>
2015-04-19 23:06:30 -0400

    citibike download script

---
+++ download_citibike_trips.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#
+# Description:
+#   Fetches trip files from the Citibike site http://www.citibikenyc.com/system-data
+#   e.g., https://s3.amazonaws.com/tripdata/201407-citibike-tripdata.zip
+#
+# Usage: ./download_citibike_trips.sh
+#
+# Requirements: curl or wget
+#
+# Source: Adapted from Jake Hofman's https://github.com/jhofman/msd2015/blob/master/lectures/lecture_2/download_trips.sh
+#
+
+# Set a relative path for the Citibike data (uses current directory by default)
+DATA_DIR=.
+
+# Retrieve list of all 2014 trip data file urls
+urls=`curl 'http://www.citibikenyc.com/system-data' | grep '2014.*tripdata.zip' | cut -d'"' -f2` # all 2014 urls
+# urls=`curl 'http://www.citibikenyc.com/system-data' | grep '201401\|201402.*tripdata.zip' | cut -d'"' -f2` # test urls
+
+# Change to the data directory
+cd $DATA_DIR
+
+# Loop over each month
+for url in $urls
+do
+    # Download the zip file
+    curl -O $url
+
+    # Define local file names
+    file=`basename $url`
+    csv=${file//.zip/}".csv"
+
+    # Unzip the downloaded file, remove header lines, include only relevant columns,
+    # remove minute and second from timestamp, group/count each unique line, and save as csv
+    unzip -p $file | sed 1d | cut -d, -f2,4,15 | sed 's/:[0-9][0-9]:[0-9][0-9]//g' | sort | uniq -c > $csv
+
+    # Remove the zip file
+    rm $file
+done
+
+# Concatenate montly files into one file
+cat $(ls *tripdata.csv) > all_2014_trips.csv
+
+# Remove the montly csv files
+rm *tripdata.csv
+
+# Output in the form
+# count "starttime","start station id","gender"
+# 1 "2014-01-01 00","151","1"
+
